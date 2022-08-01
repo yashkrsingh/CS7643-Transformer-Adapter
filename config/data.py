69b830ba151fe92fa04c3354c27f2c3c4b03e6fd
Singh, Yash Kumar <yash_kumar_singh@dell.com>
2022-08-02 00:35:35 +0530

    Added Base Files

---
+++ config/data.py
@@ -0,0 +1,21 @@
+
+DATASETS = {
+    "rct-20k": {
+        "data_dir_train": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/rct-20k/train.jsonl",
+        "data_dir_dev": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/rct-20k/dev.jsonl",
+        "data_dir_test": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/rct-20k/test.jsonl",
+        "dataset_size": 180040
+    },
+    "rct-sample": {
+        "data_dir_train": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/rct-sample/train.jsonl",
+        "data_dir_dev": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/rct-sample/dev.jsonl",
+        "data_dir_test": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/rct-sample/test.jsonl",
+        "dataset_size": 500
+    },
+    "citation_intent": {
+        "data_dir_train": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/citation_intent/train.jsonl",
+        "data_dir_dev": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/citation_intent/dev.jsonl",
+        "data_dir_test": "https://s3-us-west-2.amazonaws.com/allennlp/dont_stop_pretraining/data/citation_intent/test.jsonl",
+        "dataset_size": 1688
+    }
+}
